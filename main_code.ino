81d3907054e796d6ed5b7789d1a15ac27ee49dc6
justin-fe <188651274+justin-fe@users.noreply.github.com>
2025-05-30 17:46:35 -0400

    finished code

--- main_code.ino
+++ main_code.ino
@@ -1,2 +1,84 @@
-put the code here
-to be fixed
+#define in1 5 //L298n Motor Driver pins.
+#define in2 6
+#define in3 10
+#define in4 11
+#define LED 13
+int command; //Int to store app command state.
+int Speed = 180; // 0 - 255.
+int Speedsec;
+int buttonState = 0;
+int lastButtonState = 0;
+int Turnradius = 0; //Set the radius of a turn, 0 - 255 Note:the robot will malfunction if this is higher than int Speed.
+int brakeTime = 45;
+int brkonoff = 1; //1 for the electronic braking system, 0 for normal.
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT); 
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(LED, OUTPUT); //Set the LED pin.
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+}
+ 
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); //Initialize with motors stoped.
+    switch (command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+      case 'G':
+        forwardleft();
+        break;
+      case 'I':
+        forwardright();
+        break;
+    }
+
+  }
+}
+ 
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
+}
+ 
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
+}
+ 
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed); 
+}
+ 
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
+}
+void forwardleft() {
+  analogWrite(in1, Speedsec);
+  analogWrite(in3, Speed);
+}
+void forwardright() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speedsec);
+}
+ 
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
